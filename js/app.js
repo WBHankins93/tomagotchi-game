63f9e3e9e8ce4402719c78bc250eee12ea41ddea
Ben <WBHankins93@Bens-MacBook-Air.local>
2018-09-25 14:33:47 -0500

    Class completed

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// class tomagotchi {
+//   constructor(name, age, hunger, sleepiness, boredom) {
+//     this.name = name;
+//     this.age = age;
+//     this.hunger = hunger;
+//     this.sleepiness = sleepiness;
+//     this.boredom = boredom;
+//   }
+//   activate() {
+//     console.log('Welcome ' + this.name);
+//   }
+// }
+
+class tomagotchi {
+  activate() {
+    return this;
+  }
+}
+console.log();
