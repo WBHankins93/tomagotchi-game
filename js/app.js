3dd03896968f533ef677c8d1d8b25a233ac866f8
Ben <WBHankins93@Bens-MacBook-Air.local>
2018-09-26 14:45:28 -0500

    Something works

--- js/app.js
+++ js/app.js
@@ -1,19 +1,70 @@
-// class tomagotchi {
-//   constructor(name, age, hunger, sleepiness, boredom) {
-//     this.name = name;
-//     this.age = age;
-//     this.hunger = hunger;
-//     this.sleepiness = sleepiness;
-//     this.boredom = boredom;
-//   }
-//   activate() {
-//     console.log('Welcome ' + this.name);
-//   }
-// }
+//-----Name-------
 
-class tomagotchi {
-  activate() {
-    return this;
+//----Tamagotchi class
+class tamagotchi {
+  constructor(hunger, sleepiness, boredom, age) {
+    this.hunger = 10;
+    this.sleepiness = 10;
+    this.boredom = 10;
+    this.age = 0;
+    this.name = name;
   }
+};
+
+//------BUTTON ACTIVITY------
+
+//---HUNGER
+$('#feed').on('click', () => {
+  let $eat = $('#hunger').val();
+  $eat++;
+  $('#hunger').val($eat)
+  console.log($eat);
+})
+
+//-----SLEEPINESS
+$('#turnOff').on('click', () => {
+  let $lights = $('#sleepiness').val();
+  $lights++;
+  $('#sleepiness').val($lights)
+  console.log($lights);
+})
+
+//----BOREDOM
+$('#play').on('click', () => {
+  let $excercise = $('#boredom').val();
+  $excercise++;
+  $('#boredom').val($excercise)
+  console.log($excercise);
+})
+
+
+//------Register name on screen after input box------
+const submitButton = $(".button").click(function () {
+	let userName = $('#userNameInput').val();
+	$('#userDisplay').html(userName);
+});
+
+
+//----Lights function
+
+const lightsOff = () => {
+  $().css({
+    'background-color': 'grey'
+  })
 }
-console.log();
+
+//------Timer Function----
+
+const setTimer = () => {
+  const timer = setInterval(() => {
+    time--;
+
+    if(tamagotchi.age % 10 === 0) {
+      age += 10;
+
+      console.log(tomagotchi.age);
+    }
+  })
+}
+
+//------POSSIBLE DEATH-----
